be42e403fdf4396f218918e11100e0b9493508b1
fewtarius <fewtarius@steamfork.org>
2026-08-23 10:50:29 -0400

    chore: Remove diagnostic scripts from Metal Toolchain troubleshooting

--- test_dtruss.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Use dtruss to trace what the metal proxy does when looking for the Metal Toolchain
-echo 'void main() {}' | sudo dtruss -f /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/metal -c -x metal - -o /tmp/test_dtruss.air 2>&1 | grep -iE "open|stat|metal|toolchain" | head -50
-echo "EXIT: $?"
--- test_metal.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo 'void main() {}' | /var/run/com.apple.security.cryptexd/mnt/com.apple.MobileAsset.MetalToolchain-v17.3.7003.10.XioxnB/Metal.xctoolchain/usr/bin/metal -c -x metal - -o /tmp/test_cryptex.air 2>&1
-echo "EXIT: $?"
